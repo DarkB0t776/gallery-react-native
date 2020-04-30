4c18f7264dd26471875d4fdf8f4fcb43dbf0a998
Dima <dima23790@gmail.com>
2020-04-30 12:05:14 +0300

    implemented downloading image from gallery or camera

--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 const UploadScreen = () => {
+  const [image, setImage] = useState('');
+
+  const options = {
+    title: 'Select Avatar',
+    customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+  const getImage = () =>
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        setImage(source);
+      }
+    });
+
   return (
     <View>
-      <Text>Upload Screen</Text>
+      <Button title="Upload" onPress={getImage} />
+      <Image source={image} style={styles.image} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
 
 export default UploadScreen;
