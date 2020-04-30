7324f5ad10414814648326b7a5b442266b3c8c77
Dima <dima23790@gmail.com>
2020-04-30 11:28:03 +0300

    implemented splash screen

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View, Text} from 'react-native';
 import UploadScreen from './src/screens/UploadScreen';
 import ImagesScreen from './src/screens/ImagesScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
